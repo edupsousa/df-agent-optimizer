1d3bb63316074e9edb689418f783001dff4e02c0
Eduardo P de Sousa <edupsousa@gmail.com>
2021-03-12 16:10:23 -0300

    separating pages and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Redirect, Route, Switch } from "wouter";
-import AgentImportForm from "components/AgentImportForm";
+import AgentImportForm from "pages/AgentImportForm";
 import AppNavBar from "components/AppNavBar";
-import IntentList from "components/IntentList";
-import RenameIntents from "components/RenameIntents";
+import IntentList from "pages/IntentList";
+import RenameIntents from "pages/RenameIntents";
 import useAgentStore from "hooks/useAgentStore";
 
 function App() {
