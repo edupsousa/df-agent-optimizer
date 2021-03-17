219a2fccb2d909a53592f08b2164a5e01ec62411
Eduardo P de Sousa <edupsousa@gmail.com>
2021-03-17 14:47:10 -0300

    added react-graph-vis

--- src/pages/DiagramPage.tsx
+++ src/pages/DiagramPage.tsx
@@ -1,20 +1,49 @@
 import useAgentStore from "hooks/useAgentStore";
 import { useIntents2Contexts } from "hooks/useIntents2Contexts";
 import React from "react";
+import Graph, { Options } from "react-graph-vis";
+
+const options: Options = {
+  height: "500px",
+  edges: {
+    smooth: {
+      type: "continuous",
+    },
+  },
+  physics: {
+    enabled: false,
+  },
+  layout: {
+    hierarchical: {
+      enabled: true,
+      direction: "LR",
+    },
+  },
+};
 
 export default function DiagramPage() {
   const { intentList } = useAgentStore();
   const contexts = useIntents2Contexts(intentList);
   const contextNames = Object.keys(contexts).sort();
+  if (!intentList) return null;
+  const nodes = intentList.map((i) => ({
+    id: i.intent.name,
+    label: i.intent.name,
+  }));
+  const edges = contextNames
+    .map((ctxName) =>
+      contexts[ctxName].inputOn
+        .map((to) =>
+          Object.keys(contexts[ctxName].outputOn).map((from) => ({ from, to }))
+        )
+        .flat()
+    )
+    .flat();
 
   return (
     <div>
       <h1>Diagram</h1>
-      <ul>
-        {contextNames.map((ctx, idx) => (
-          <li key={idx}>{ctx}</li>
-        ))}
-      </ul>
+      <Graph graph={{ nodes, edges }} options={options} />
     </div>
   );
 }
