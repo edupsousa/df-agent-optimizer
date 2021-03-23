02135e1d6c7cd452efd9eeedcc608a6fe641297b
Eduardo P de Sousa <edupsousa@gmail.com>
2021-03-22 21:31:06 -0300

    display input, output contexts and intent responses

--- src/components/IntentDetails.tsx
+++ src/components/IntentDetails.tsx
@@ -15,9 +15,51 @@ export default function IntentDetails({ intentName }: IntentDetailsProps) {
   if (!intent) return null;
 
   return (
-    <Row>
-      <Col>Intent:</Col>
-      <Col>{intent.name}</Col>
-    </Row>
+    <>
+      <Row>
+        <Col md={3}>Intent:</Col>
+        <Col>{intent.name}</Col>
+      </Row>
+      <Row>
+        <Col md={3}>Input Contexts:</Col>
+        <Col>
+          <ul>
+            {intent.contexts.sort().map((ctxName) => (
+              <li key={ctxName}>{ctxName}</li>
+            ))}
+          </ul>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={3}>Output Contexts:</Col>
+        <Col>
+          <ul>
+            {intent.responses.map((r) =>
+              r.affectedContexts
+                .sort(({ name: a }, { name: b }) =>
+                  a > b ? 1 : a < b ? -1 : 0
+                )
+                .map((ctx) => (
+                  <li key={ctx.name}>
+                    {ctx.name} ({ctx.lifespan})
+                  </li>
+                ))
+            )}
+          </ul>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={3}>Reponses</Col>
+        <Col>
+          <ul>
+            {intent.responses.map((r) =>
+              r.messages
+                .filter((m) => m.speech)
+                .map((m, i) => <li key={i}>{m.speech}</li>)
+            )}
+          </ul>
+        </Col>
+      </Row>
+    </>
   );
 }
